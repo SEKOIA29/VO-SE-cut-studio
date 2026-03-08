d9ac4b1376a2e5e86d541ef9df57b6f118a04e4b
セコイア <sekoia2900@gmail.com>
2026-03-08 19:35:07 +0900

    Update __init__.py

--- modules/talk/__init__.py
+++ modules/talk/__init__.py
@@ -1,5 +1,8 @@
 # modules/talk/__init__.py
-from .intonation_analyzer import IntonationAnalyzer
-from .talk_manager import TalkManager, generate_talk_events
+from .vo_se_engine import (
+    IntonationAnalyzer,
+    TalkManager,
+    generate_talk_events
+)
 
 __all__ = ["IntonationAnalyzer", "TalkManager", "generate_talk_events"]
